6837e0573c302dfeec079067a437d9b989de3c45
liu <769601722@qq.com>
2023-08-03 15:29:25 +0800

    update

--- preload.js
+++ preload.js
@@ -2,9 +2,11 @@ const { contextBridge, ipcRenderer } = require("electron");
 const fs = require("fs");
 const path = require("path");
 const database = require("./database/service");
-const readme = fs.readFileSync("README.md", { encoding: "utf-8" });
-const BD_APP_KEY = 'uUDl9eiBcGG1YAaIeF6u97VI7fCkWAEt';
-const BD_APP_SECRET_Key = 'n7Regie4NzFzL109MZgM23CgrH7LYrnv';
+const readme = fs.readFileSync(path.join(__dirname, "README.md"), {
+  encoding: "utf-8",
+});
+const BD_APP_KEY = "uUDl9eiBcGG1YAaIeF6u97VI7fCkWAEt";
+const BD_APP_SECRET_Key = "n7Regie4NzFzL109MZgM23CgrH7LYrnv";
 contextBridge.exposeInMainWorld("appContext", {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
@@ -16,5 +18,5 @@ contextBridge.exposeInMainWorld("appContext", {
   cwd: () => process.cwd(),
   readme,
   BD_APP_KEY,
-  BD_APP_SECRET_Key
+  BD_APP_SECRET_Key,
 });
